911fb73468ad681cfd1550b1fbc7520e836ae0d1
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-06-24 20:23:27 -0300

    Update SetTurn.tsx

--- src/context/SetTurn.tsx
+++ src/context/SetTurn.tsx
@@ -5,14 +5,14 @@ import {ISetTurn} from "../interfaces/ISetTurn";
 import {IChildren} from "../interfaces/IChildren";
 
 export const setTurnContext = createContext({} as ISetTurn);
-const starterSimbol = localStorage.getItem("starterPlayerSimbol");
+const starterLocalCpu = localStorage.getItem("starterLocalCpu");
 
 const SetTurnProvider = ({children}: IChildren): JSX.Element => {
 	const emptyBoxes = Array(9).fill("");
 	const [boxes, setBoxes] = useState(emptyBoxes);
 	const [currentPlayer, setCurrentPlayer] = useState("x");
 	const [playerTurn, setPlayerTurn] = useState<boolean>(false);
-	const [cpuTurn, setCpuTurn] = useState<boolean>(starterSimbol === "x" ? false : true);
+	const [cpuTurn, setCpuTurn] = useState<boolean>(starterLocalCpu === "x" ? true : false);
 
 	return (
 		<setTurnContext.Provider
